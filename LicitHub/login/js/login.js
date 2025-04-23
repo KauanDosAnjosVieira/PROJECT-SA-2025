e3464216f3051659267bdd2d8ca102df85d39b17
Kauan Dos Anjos Vieira <kauaanvieiraa07@gmail.com>
2025-04-23 17:04:11 -0300

    Sa

--- LicitHub/login/js/login.js
+++ LicitHub/login/js/login.js
@@ -1,10 +1,12 @@
+function login(event) {
+    event.preventDefault(); // impede o envio do formulário
 
-/* script.js */
-function login() {
-    const user = document.getElementById('user').value;
-    const password = document.getElementById('password').value;
+    const user = document.getElementById('email').value;
+    const password = document.getElementById('senha').value;
+
     if (user && password) {
         alert('Login bem-sucedido!');
+        window.location.href = '../inicial/index.html'; // redireciona para a página inicial
     } else {
         alert('Por favor, preencha todos os campos.');
     }
