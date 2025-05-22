c48cd82c0e07a26cb71de1f942fad5d057b558a6
Lucas Santos <dossantoslucs@gmail.com>
2025-05-22 03:00:20 -0300

    Deletes

--- LicitHub/login/teste_conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '', 'licithub_db');
-
-if ($conn->connect_error) {
-    die("Conexão falhou: " . $conn->connect_error);
-} else {
-    echo "Conectado com sucesso!";
-}
-?>
