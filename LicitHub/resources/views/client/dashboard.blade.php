1df17dd80c4afaad2d7c6eed607612e727a59e3c
Kauan Dos Anjos Vieira <kauaanvieiraa07@gmail.com>
2025-07-02 00:52:22 -0300

    att

--- LicitHub/resources/views/client/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- filepath: resources/views/client/dashboard.blade.php --}}
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <h1>Dashboard do Cliente</h1>
-        <p>Bem-vindo ao painel do cliente!</p>
-    </div>
-@endsection
