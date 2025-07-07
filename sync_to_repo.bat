f323ebefe268ed5b361527a6103a9b2f321373c3
Kauan Dos Anjos Vieira <kauaanvieiraa07@gmail.com>
2025-07-07 16:24:03 -0300

    att

--- sync_to_repo.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-xcopy /E /H /Y /I "C:\xampp\htdocs\LicitHub_PHP" "C:\Users\kauan_a_vieira\Documents\GitHub\PROJECT-SA-2025\LicitHub_PHP"
-cd "C:\Users\kauan_a_vieira\Documents\GitHub\PROJECT-SA-2025"
-git add .
-git commit -m "Atualizando conteudo do Projeto FInal"
-git push origin main
-pause
